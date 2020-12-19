ff0f9643b191c1521ec82888e3e7b49eb9da78df
Owen Salter <owen@devosmium.xyz>
2020-12-19 16:39:21 -0500

    Allow users to get help for a specific command (#3)
    * Start adding the ability to get help for specific command
    
    Issue #2
    
    * Add checks to make output more readable
    
    Replace 'undefined' in aliases with 'None'
    
    Replace 'public' in Permissions with 'None' and anything else with 'Restricted'
    
    * Replace commands.forEach with getCommand
    
    * Add check to see if no commands were found

--- triggers/commands/help.ts
+++ triggers/commands/help.ts
@@ -2,12 +2,31 @@ import { MessageEmbed, EmbedFieldData } from "discord.js";
 import { Command } from "../../Types";
 import { commands, config } from "../..";
 import { hasPermission } from "../../Permissions";
+import { getCommand } from "../../util/commands";
+
 
 module.exports = <Command> {
     name: 'help',
     description: 'Send Help Message',
-    async execute(message) {
-
+    minArgs: 0,
+    async execute(message, args) {
+        
+        if (args.length > 0) {
+            let command = getCommand(args[0]);
+            if (!command) {
+                message.channel.send("No matching commands found!")
+                return;
+            }
+            const embed = new MessageEmbed()
+                .setColor("#AC2B37")
+                .setTitle(command.name)
+                .setDescription(command.description)
+                .addField("Aliases", `${command.aliases ? command.aliases : 'None'}`)
+                .addField("Required Permissions", `${command.requiredPerms == 'public' ? 'None' : 'Restricted'}`)
+            message.channel.send(embed)
+                
+        } else {
+        
         let embedFields = new Array<EmbedFieldData>();
         commands.forEach(command => {
             if (hasPermission(message, command)) {
@@ -31,5 +50,6 @@ module.exports = <Command> {
             .addFields(embedFields);
         
         message.channel.send(helpEmbed);
+        }
     }
 }
