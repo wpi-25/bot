faaf45a839a4a764f68821d7547a805d2158355a
Sam <centersam19@gmail.com>
2021-02-08 13:49:36 -0500

    Overhaul start scripts
    Now on restart, scripts for both platforms check if there was an update since the last restart and if so, remember it and check if there is a need to update dependencies

---
+++ start.ps1
@@ -0,0 +1,30 @@
+﻿Write-Output "Starting"
+while ($true) {
+    npm start
+    if ($?) { # If we updated successfully
+        Write-Output "Restarting"
+        if ($args[0] -eq "--no-update-check") {
+            Write-Output "Not checking for updates"
+            continue
+        }
+
+        # Attempt to update dependencies
+        $COMMITFILE = ".\.commit"
+        try {
+            $LAST_COMMIT = Get-Content -Path $COMMITFILE
+        } catch {
+            $LAST_COMMIT = ""
+        }
+        $LATEST_COMMIT = @(git show)[0].substring(7, 7)
+        if ((Test-Path $COMMITFILE) -and $LAST_COMMIT -eq $LATEST_COMMIT) {
+            continue
+        }
+        Write-Output "Saving commit to file"
+        Out-File -FilePath $COMMITFILE -InputObject $LATEST_COMMIT
+        if (@(git diff-tree --no-commit-id --name-only -r $LATEST_COMMIT) -contains "package.json") {
+            npm install
+        }
+    } else {
+        break;
+    }
+}
--- start.sh
+++ start.sh
@@ -1,12 +1,27 @@
-while :
-do
-    if ts-node index
-    then
-        echo "restarting"
-        continue
+while true; do # Loop forever
+    if npm start; then   # If we exited successfully
+        echo "Restarting"
+        if [[ "$1" == "--no-update-check" ]]; then
+            echo "Not checking for updates"
+            continue    # Restart the loop
+        fi
+        # Attempt to update dependencies
+        COMMITFILE=./.commit
+        LATESTCOMMIT=`git show | grep commit | cut -c8-14`
+        echo "Last commit:  " `cat $COMMITFILE`
+        echo "Latest commit: $LATESTCOMMIT"
+        if [ -f "$COMMITFILE" ] && [ `cat $COMMITFILE` == "$LATESTCOMMIT" ]; then  # We're up to date
+            continue    # Restart the loop
+        else
+            echo "Saving commit to file"
+            echo $LATESTCOMMIT > $COMMITFILE # Save the commit we're on now
+            # If package.json was modified
+            if git diff-tree --no-commit-id --name-only -r $LATESTCOMMIT | grep package.json
+            then
+                npm install # Install updated/new dependencies
+            fi
+        fi
     else
         break
     fi
 done
-echo "crashed"
-read -n 1 -s -r -p "Press any key to continue"
