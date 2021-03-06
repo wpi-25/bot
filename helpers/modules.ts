70ac90c77b8b44235587d9ab5b79c5297c16b7cd
Sam <centersam19@gmail.com>
2021-03-06 16:49:49 -0500

    Queue level updates on bot startup (#79)
    * Theoretically update levels on boot
    
    * Rework Queue
    
    Instead of running through all members on boot (because it doesn't work), level updates are queued so a user's level is updated on that their next message

--- helpers/modules.ts
+++ helpers/modules.ts
@@ -8,12 +8,19 @@ const commandFiles = readdirSync('./triggers/commands').filter((file) =>
     file.endsWith('.ts')
 );
 for (const file of commandFiles) {
-    console.log(`Importing command ${file}`);
-    const command: Command = require(`../triggers/commands/${file}`);
-    commands.set(command.name, command);
-    console.log(`Imported command ${command.name}`);
+    // console.log(`Importing command ${file}`);
+    try {
+        const command: Command = require(`../triggers/commands/${file}`);
+        commands.set(command.name, command);
+        console.log(`Imported command ${command.name}`);
+    } catch (e) {
+        console.warn(
+            `[WARN]\tCould not import command ${file.slice(0, -3)}\n`,
+            e
+        );
+    }
 }
-console.log('Imported commands\n');
+// console.log('Imported commands\n');
 
 export const triggers = new Collection<string, TriggeredCommand>();
 const triggerFiles = readdirSync('./triggers/triggers').filter((file) =>
@@ -21,12 +28,16 @@ const triggerFiles = readdirSync('./triggers/triggers').filter((file) =>
 );
 for (const file of triggerFiles) {
     const name = file.slice(0, -3);
-    console.log(`Importing trigger ${file}`);
-    const trigger: TriggeredCommand = require(`../triggers/triggers/${file}`);
-    triggers.set(name, trigger);
-    console.log(`Imported trigger ${name}`);
+    // console.log(`Importing trigger ${file}`);
+    try {
+        const trigger: TriggeredCommand = require(`../triggers/triggers/${file}`);
+        triggers.set(name, trigger);
+        console.log(`Imported trigger ${name}`);
+    } catch (e) {
+        console.warn(`[WARN]\tCould not import trigger ${name}\n`, e);
+    }
 }
-console.log('Imported triggers\n');
+// console.log('Imported triggers\n');
 
 export const reactions = new Collection<string, ReactionCommand>();
 const reactionFiles = readdirSync('./triggers/reactions').filter((file) =>
@@ -34,9 +45,13 @@ const reactionFiles = readdirSync('./triggers/reactions').filter((file) =>
 );
 for (const file of reactionFiles) {
     const name = file.slice(0, -3);
-    console.log(`Importing reaction command ${file}`);
-    const reaction: ReactionCommand = require(`../triggers/reactions/${file}`);
-    reactions.set(name, reaction);
-    console.log(`Imported reaction command ${name}`);
+    // console.log(`Importing reaction command ${file}`);
+    try {
+        const reaction: ReactionCommand = require(`../triggers/reactions/${file}`);
+        reactions.set(name, reaction);
+        console.log(`Imported reaction command ${name}`);
+    } catch (e) {
+        console.warn(`[WARN]\tCould not import trigger ${name}\n`, e);
+    }
 }
-console.log('Imported reaction commands\n');
+// console.log('Imported reaction commands\n');
